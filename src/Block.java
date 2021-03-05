47bbc62c8a16cc9146c86e48cf0ce984967b29ba
eunicepang <34784157+eunicepang@users.noreply.github.com>
2021-03-05 12:02:05 -0800

    Update Block.java

--- src/Block.java
+++ src/Block.java
@@ -1,16 +1,27 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 
 public class Block {
     
+    private Date timestamp; 
     private int previousHash;
     private int currentHash;
     private ArrayList<Transaction> transactions;
 
-    public Block(int previousHash, ArrayList<Transaction> transactions){
+    public Block(Date timestamp, int previousHash, ArrayList<Transaction> transactions){
+        this.timestamp = timestamp;
         this.previousHash = previousHash;
         this.currentHash = Arrays.hashCode(new int[] {transactions.hashCode() ,this.previousHash});
         this.transactions = transactions;
+    }
+
+    public Date getTimestamp(){
+        return this.timestamp;
+    }
+
+    public void setTimestamp(Date timestamp){
+        this.timestamp = timestamp;
     }
 
     public int getPreviousHash(){
